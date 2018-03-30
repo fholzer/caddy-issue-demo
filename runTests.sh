e39a33fef8d8a5818b1673181d300d4bdd3f4d53
Ferdinand Holzer <ferdinand.holzer@gmail.com>
2018-03-30 02:10:33 +0200

    Added a test scenario

--- runTests.sh
+++ runTests.sh
@@ -2,9 +2,16 @@
 CACERT=/opt/easyrsa/pki/ca.crt
 
 runTests() {
+	runTestsUnprotected
+	runTestsProtected
+}
+
+runTestsUnprotected() {
 	echo -n "Should not require client authentication on unprotected vhost..."
 	curl -s --cacert $CACERT --resolve unprotected.example.com:443:127.0.0.1 https://unprotected.example.com/ | grep -q "everyone" && echo "OK" || echo "FAILED"
+}
 
+runTestsProtected() {
 	echo -n "Should require client authentication on protected vhost..."
 	curl -s -f --cacert $CACERT --resolve protected.example.com:443:127.0.0.1 https://protected.example.com/ 1>/dev/null 2>&1
 	[ "x$?" == "x35" ] && echo "OK" || echo "FAILED"
@@ -18,8 +25,8 @@ echo
 echo Test tls client authentication
 echo
 echo Suite 1
+echo unprotected server appears first in Caddyfile, then protected server
 
-# test 1
 :> Caddyfile
 cat Caddyfile.part.unprotected >> Caddyfile
 cat Caddyfile.part.protected >> Caddyfile
@@ -35,8 +42,8 @@ sleep 1
 
 echo
 echo Suite 2
+echo protected server appears first in Caddyfile, then unprotected server
 
-# test 2
 :> Caddyfile
 cat Caddyfile.part.protected >> Caddyfile
 cat Caddyfile.part.unprotected >> Caddyfile
@@ -46,5 +53,21 @@ cat Caddyfile.part.unprotected >> Caddyfile
 CPID=$!
 sleep 1
 runTests
+kill $CPID
+sleep 1
+
+
+echo
+echo Suite 3
+echo only protected server is in Caddyfile
+
+:> Caddyfile
+cat Caddyfile.part.protected >> Caddyfile
+
+# run server
+./caddy -conf Caddyfile  &
+CPID=$!
+sleep 1
+runTestsProtected
 kill $CPID
 sleep 1
